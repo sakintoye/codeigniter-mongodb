42d65c737581475483527246c14dd44dfbd31737
Ola Akintoye <olasunkanmiakintoye@gmail.com>
2016-12-28 14:24:51 -0500

    Updated database connection file

--- config/mongo_db.php
+++ config/mongo_db.php
@@ -1,4 +1,18 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+/**
+* CodeIgniter Active Record Library for MongoDb
+*
+* Connection strings for connecting to MongoDb in Code Igniter
+*
+* @package CodeIgniter
+* @author Ola Akintoye | http://akintoye.me | GitHub: @sakintoye
+* @copyright Copyright (c) 2016, Sunkanmi Akintoye.
+* @license http://www.opensource.org/licenses/mit-license.php
+* @link https://github.com/sakintoye/codeigniter-mongodb
+* @version Version 1.0
+* Thanks to Intekhab Rizvi whose library inspired this project.
+*/
+
 /*
 | -------------------------------------------------------------------------
 | DATABASE CONNECTIVITY SETTINGS
@@ -39,19 +53,4 @@ $config['mongo_db']['default']['read_preference'] = NULL;
 $config['mongo_db']['default']['read_preference_tags'] = NULL;
 
 
-/*
-$config['mongo_db']['test']['no_auth'] = FALSE;
-$config['mongo_db']['test']['hostname'] = 'localhost';
-$config['mongo_db']['test']['port'] = '27017';
-$config['mongo_db']['test']['username'] = 'username';
-$config['mongo_db']['test']['password'] = 'password';
-$config['mongo_db']['test']['database'] = 'database';
-$config['mongo_db']['test']['db_debug'] = TRUE;
-$config['mongo_db']['test']['return_as'] = 'array';
-$config['mongo_db']['test']['write_concerns'] = (int)1;
-$config['mongo_db']['test']['journal'] = TRUE;
-$config['mongo_db']['test']['read_preference'] = NULL;
-$config['mongo_db']['test']['read_preference_tags'] = NULL;
-*/
-/* End of file database.php */
-/* Location: ./application/config/database.php */
+/* Location: ./application/config/mongo_db.php */
